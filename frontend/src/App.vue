95f514ebf5f18b037ee1e35490833f5babf6d87b
Maryam-bit <rajputmaryam5555@gmail.com>
2023-07-30 23:58:52 +0500

    feat: debouncing completed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,9 +3,9 @@
     <div>
       <h1>Blazing Fast Search Engine</h1>
       <label for="search">Search</label>
-      <input type="text" v-model="searchQuery">
+      <input type="text" v-model="searchQuery" @input="onInput">
       <h1 v-if="loading">Loading....</h1>
-      <ul v-else="!loading">
+      <ul v-else>
         <li v-for="book in books" :key="book.id">{{ book.book_title }}</li>
       </ul>
     </div>
@@ -14,26 +14,40 @@
 
 <script setup lang="ts">
 import axios from "axios";
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import debounce from 'lodash.debounce'
 
+// ============ Variable ==============
 const searchQuery = ref("");
 const books = ref([]);
 const loading = ref(false);
-const fetchBooks = async () => {
+
+// ============ Methods ==============
+const fetchBooks = async (searchKey: string) => {
   try {
     loading.value = true;
     const response = await axios({
       method: 'get',
       url: 'http://localhost:3000/api/books',
+      params: {
+        searchKey
+      }
     });
     books.value = response.data.data;
-    console.log(books.value, "bbooks")
+    console.log(books.value, "books")
     loading.value = false;
   } catch (error) {
     console.error(error);
     loading.value = false;
   }
 };
+fetchBooks(searchQuery.value);
 
-fetchBooks();
+// Debounce the fetchBooks function with 500ms delay
+const debouncedFetchBooks = debounce(fetchBooks, 4000);
+
+// ============ Watcher ==============
+watch(searchQuery, (newValue, oldValue) => {
+  debouncedFetchBooks(newValue);
+})
 </script>
