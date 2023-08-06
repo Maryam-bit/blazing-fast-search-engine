254b5f2de610cefcc31104a9cacb0dbd6050e594
Maryam-bit <rajputmaryam5555@gmail.com>
2023-08-06 20:09:20 +0500

    feat: get count of records

--- backend/src/controllers/books.controller.ts
+++ backend/src/controllers/books.controller.ts
@@ -13,15 +13,17 @@ const pool = new Pool({
 
 export const getBooks = async (req: Request, res: Response) => {
   try {
-    const searchKey = req.query.searchKey;
+    const searchKey = req.query.searchKey || "";
     const cursor = req.query.cursor || 0;
     const limit = req.query.limit || 10;
+    let count;
+
 
     // Base query string
     let query = `SELECT * FROM "Books" WHERE`;
 
     // Check if the searchKey exists and add the appropriate search condition
-    if (searchKey) {
+    if (searchKey.length) {
       query += ` (book_author ILIKE '%${searchKey}%' OR book_title ILIKE '%${searchKey}%') AND`;
     }
 
@@ -29,8 +31,16 @@ export const getBooks = async (req: Request, res: Response) => {
     query += ` id > ${cursor} ORDER BY id ASC LIMIT ${limit}`;
 
     const { rows } = await pool.query(query);
+
+    // get count
+    if(!searchKey.length && cursor == 0) {
+      console.log(true)
+      count = await getCountOfBooks();
+    }
+    
     const nextCursor = rows.length > 0 ? rows[rows.length - 1].id : null;
-    res.status(httpStatus.OK).json({ data: rows, nextCursor });
+
+    res.status(httpStatus.OK).json({ data: rows, nextCursor, count });
   } catch (error) {
     console.log(error, "ERROR EXECUTING SQL QUERY");
     res
@@ -38,3 +48,15 @@ export const getBooks = async (req: Request, res: Response) => {
       .json({ error: "Internal server error" });
   }
 };
+
+export const getCountOfBooks = async () => {
+  try {
+    const countQuery = `SELECT COUNT(*) AS total FROM "Books"`;
+    const countResult = await pool.query(countQuery);
+    const totalCount = countResult.rows[0].total;
+
+    return totalCount;
+  } catch (error) {
+    console.log("error while getting count")
+  }
+}
