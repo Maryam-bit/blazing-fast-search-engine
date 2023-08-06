c5476ac636b9b48b1770a240048cbb352a4ba8b2
Maryam-bit <rajputmaryam5555@gmail.com>
2023-08-06 20:16:05 +0500

    fix: removed unused code

--- frontend/src/components/BookListItemSkeleton.vue
+++ frontend/src/components/BookListItemSkeleton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-4 border-b border-gray-300 ">
+  <div class="py-4 border-b border-gray-300">
     <div class="animate-pulse flex flex-col space-y-3">
       <div class="w-[70%] h-6 bg-gray-300 rounded-md"></div>
       <div class="w-32 h-4 bg-gray-300 rounded-md"></div>
